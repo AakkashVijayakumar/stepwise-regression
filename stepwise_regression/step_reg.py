1b90b403cbccd784e815007f26f12c6f3f76356d
AakkashVijayakumar <47106771+AakkashVijayakumar@users.noreply.github.com>
2019-02-11 20:33:11 +0530

    Add files via upload

---
+++ stepwise_regression/step_reg.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import statsmodels.api as sm
+
+
+def forward_regression(X, y,
+                       threshold_in,
+                       verbose=False):
+    initial_list = []
+    included = list(initial_list)
+    while True:
+        changed=False
+        excluded = list(set(X.columns)-set(included))
+        new_pval = pd.Series(index=excluded)
+        for new_column in excluded:
+            model = sm.OLS(y, sm.add_constant(pd.DataFrame(X[included+[new_column]]))).fit()
+            new_pval[new_column] = model.pvalues[new_column]
+        best_pval = new_pval.min()
+        if best_pval < threshold_in:
+            best_feature = new_pval.idxmin()
+            included.append(best_feature)
+            changed=True
+            if verbose:
+                print('Add  {:30} with p-value {:.6}'.format(best_feature, best_pval))
+
+        if not changed:
+            break
+
+    return included
+
+def backward_regression(X, y,
+                           threshold_out,
+                           verbose=False):
+    included=list(X.columns)
+    while True:
+        changed=False
+        model = sm.OLS(y, sm.add_constant(pd.DataFrame(X[included]))).fit()
+        # use all coefs except intercept
+        pvalues = model.pvalues.iloc[1:]
+        worst_pval = pvalues.max() # null if pvalues is empty
+        if worst_pval > threshold_out:
+            changed=True
+            worst_feature = pvalues.idxmax()
+            included.remove(worst_feature)
+            if verbose:
+                print('Drop {:30} with p-value {:.6}'.format(worst_feature, worst_pval))
+        if not changed:
+            break
+    return included
+
